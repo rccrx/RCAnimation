9723c122832b72162bad2701fa7199a2732f66ee
rccrx <rccrx@qq.com>
2021-09-27 08:37:13 +0800

    added：添加Core Animation动画

--- RCAnimation/CoreAnimation/RCCoreAnimationController.m
+++ RCAnimation/CoreAnimation/RCCoreAnimationController.m
@@ -9,6 +9,7 @@
 
 #define TempLayerName @"D" /**< 重置视图时应该移除的图层的name */
 #define TempViewTag 100 /**< 重置视图时应该移除的视图的tag */
+#define TempAnimationKey @"K" /**< 动画的key */
 
 @interface RCCoreAnimationController () <CALayerDelegate>
 
@@ -45,6 +46,12 @@
     [self selfViewAddButtonWithOriginY:350 leftOrRight:NO title:@"CAScrollLayer" tag:5];
     [self selfViewAddButtonWithOriginY:400 leftOrRight:YES title:@"CATiledLayer" tag:6];
     [self selfViewAddButtonWithOriginY:400 leftOrRight:NO title:@"CAReplicatorLayer" tag:7];
+    [self selfViewAddButtonWithOriginY:450 leftOrRight:YES title:@"CABasicAnimation" tag:8];
+    [self selfViewAddButtonWithOriginY:450 leftOrRight:NO title:@"CAKeyframeAnimation" tag:9];
+    [self selfViewAddButtonWithOriginY:500 leftOrRight:YES title:@"CASpringAnimation" tag:10];
+    [self selfViewAddButtonWithOriginY:500 leftOrRight:NO title:@"CATransition" tag:11];
+    [self selfViewAddButtonWithOriginY:550 leftOrRight:YES title:@"CAAnimationGroup" tag:12];
+    [self selfViewAddButtonWithOriginY:550 leftOrRight:NO title:@"CADisplayLink或其他" tag:13]; // 修改buttonClicked的”case 13:“的代码
 }
 
 /** 按钮点击事件 */
@@ -92,6 +99,38 @@
             [self testCAReplicatorLayer];
             break;
         }
+        case 8:
+        {
+            [self testCABasicAnimation];
+            break;
+        }
+        case 9:
+        {
+            [self testCAKeyframeAnimation];
+            break;
+        }
+        case 10:
+        {
+            [self testCASpringAnimation];
+            break;
+        }
+        case 11:
+        {
+            [self testCATransition];
+            break;
+        }
+        case 12:
+        {
+            [self testCAAnimationGroup];
+            break;
+        }
+        case 13:
+        {
+//            [self testCADisplayLink];
+//            [self testCAMediaTimingFunction];
+            [self testCalculationMode];
+            break;
+        }
             
         default:
             break;
@@ -110,6 +149,8 @@
     
     UIView *subview = [self.view viewWithTag:TempViewTag];
     [subview removeFromSuperview];
+    
+    NSLog(@"%@", [self.view.layer animationForKey:TempAnimationKey]);
 }
 
 /** 测试CALayer和CAShapeLayer */
@@ -278,6 +319,170 @@
     layer.instanceTransform = CATransform3DMakeTranslation(20, 0, 0); // 第k个实例相对于第k-1的偏移，不设置时是重合的
 //    layer.instanceDelay = 2; // 给sublayer加动画时才生效
     [self.view.layer addSublayer:layer];
+}
+
+- (void)testCABasicAnimation {
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    animation.fromValue = @1;
+    animation.toValue = @0;
+    animation.duration = 1;
+    animation.repeatCount = 3;
+    animation.removedOnCompletion = YES; // YES表示动画结束之后移除，移除之后使用animationForKey获取不到这个动画；但是，使用dispatch_after会获取到，点击其他按钮调用animationForKey获取不到，也许是因为要等到下一个runloop才移除动画？
+    [self.view.layer addAnimation:animation forKey:TempAnimationKey];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSLog(@"%@", [self.view.layer animationForKey:TempAnimationKey]);
+    });
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSLog(@"%@", [self.view.layer animationForKey:TempAnimationKey]);
+    });
+}
+
+- (void)testCAKeyframeAnimation {
+    CALayer *layer = [CALayer layer];
+    layer.name = TempLayerName;
+    layer.frame = CGRectMake(20, 50, 10, 10);
+    layer.backgroundColor = UIColor.redColor.CGColor;
+    [self.view.layer addSublayer:layer];
+    
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathMoveToPoint(path, NULL, 20, 50); // 其实layer的初始position是(25, 55)，所以设置点为(20, 50)会导致动画结束时layer的位置从(20, 50)跳到(25, 55)
+    CGPathAddLineToPoint(path, NULL, 120, 50);
+    CGPathAddLineToPoint(path, NULL, 120, 150);
+    CGPathAddLineToPoint(path, NULL, 20, 150);
+    CGPathAddLineToPoint(path, NULL, 20, 50);
+    
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    //    animation.path = CGPathCreateWithRoundedRect(CGRectMake(20, 50, 200, 200), 100, 100, nil); // 默认calculationMode是linear，不设置keyTimes，这个path会在四个关键点有停顿
+    animation.path = path;
+    animation.duration = 5;
+    animation.keyTimes = @[@0, @0.25, @0.5, @0.75, @1]; // 元素数量和path控制点数量一样
+    [layer addAnimation:animation forKey:TempAnimationKey];
+}
+
+- (void)testCASpringAnimation {
+    CALayer *layer = [CALayer layer];
+    layer.name = TempLayerName;
+    layer.frame = CGRectMake(20, 50, 10, 10);
+    layer.backgroundColor = UIColor.redColor.CGColor;
+    [self.view.layer addSublayer:layer];
+    
+    CASpringAnimation *animation = [CASpringAnimation animationWithKeyPath:@"position"];
+    animation.duration = 1;
+    animation.damping = 2;
+    animation.fromValue = [NSValue valueWithCGPoint:CGPointMake(25, 55)];
+    animation.toValue = [NSValue valueWithCGPoint:CGPointMake(150, 55)];
+    animation.fillMode = kCAFillModeForwards; // 动画结束时使layer保持终止状态，xxForwards必须设置removedOnCompletion为NO才生效
+    animation.removedOnCompletion = NO;
+    [layer addAnimation:animation forKey:TempAnimationKey];
+}
+
+- (void)testCATransition {
+    CALayer *layer = [CALayer layer];
+    layer.name = TempLayerName;
+    layer.frame = CGRectMake(20, 50, 300, 150);
+    layer.backgroundColor = UIColor.redColor.CGColor;
+    [self.view.layer addSublayer:layer];
+    
+    CATransition *transition = [CATransition animation];
+    transition.duration = 1;
+    transition.type = kCATransitionPush;
+    transition.subtype = kCATransitionFromLeft;
+    [self.view.layer addAnimation:transition forKey:TempAnimationKey]; // self.view.layer图层的变化会有从右往左显露的效果，比如从无红色子图层layer到有
+}
+
+- (void)testCAAnimationGroup {
+    CALayer *sublayer = [CALayer layer];
+    sublayer.frame = CGRectMake(0, 0, 50, 50);
+    sublayer.cornerRadius = CGRectGetWidth(sublayer.frame) * 0.5;
+    sublayer.backgroundColor = [UIColor redColor].CGColor;
+    sublayer.borderWidth = 1;
+
+    
+    CABasicAnimation *alpha = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    alpha.fromValue = @1;
+    alpha.toValue = @0;
+    
+    CABasicAnimation *scale = [CABasicAnimation animationWithKeyPath:@"transform"];
+    scale.fromValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(1, 1, 1)];
+    scale.toValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(2, 2, 2)];
+    
+    CAAnimationGroup *animationGroup = [CAAnimationGroup animation];
+    animationGroup.animations = @[alpha, scale];
+    animationGroup.duration = 3;
+    [sublayer addAnimation:animationGroup forKey:TempAnimationKey];
+    
+    
+    CAReplicatorLayer *layer = [CAReplicatorLayer layer];
+    layer.name = TempLayerName;
+    layer.frame = CGRectMake(120, 100, 300, 150);
+    [layer addSublayer:sublayer];
+    layer.instanceCount = 2;
+    layer.instanceDelay = 1; // 给sublayer加动画时才生效
+    [self.view.layer addSublayer:layer];
+}
+
+- (void)testCADisplayLink {
+    CADisplayLink *displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(displayLinkSelector:)];
+    displayLink.preferredFramesPerSecond = 30; // 如果不设置，则偏好帧率等于显示的最大刷新率即maximumFramesPerSecond
+    [displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
+}
+
+- (void)displayLinkSelector:(CADisplayLink *)sender {
+    // 这个方法的调用间隔时间由preferredFramesPerSecond控制
+    // duration是每帧持续时间，但这个时间不是实际的帧持续时间，而是maximumFramesPerSecond（即60）的帧之间的时间，等于0.016667
+    // 实际的帧持续时间，用targetTimestamp-timestamp
+    // timestamp上一帧显示的时间，targetTimestamp下一帧显示的时间
+    // targetTimestamp-CACurrentMediaTime表示系统提供了这么多时间可用于渲染下一帧，如果这段时间内无法提供下一帧，则会导致丢帧。所以可以设置preferredFramesPerSecond使用低帧率，一个较慢但是连续的帧率看起来会比丢帧更平缓。
+    NSLog(@"[%@ : %@], duration=%f, timestamp=%f, targetTimestamp=%f, targetTimestamp-timestamp=%f, targetTimestamp-CACurrentMediaTime=%f", NSStringFromClass(self.class), NSStringFromSelector(_cmd),
+          sender.duration,
+          sender.timestamp,
+          sender.targetTimestamp,
+          sender.targetTimestamp - sender.timestamp,
+          sender.targetTimestamp - CACurrentMediaTime());
+}
+
+- (void)testCAMediaTimingFunction {
+    CALayer *layer = [CALayer layer];
+    layer.name = TempLayerName;
+    layer.frame = CGRectMake(80, 50, 10, 10);
+    layer.backgroundColor = UIColor.redColor.CGColor;
+    [self.view.layer addSublayer:layer];
+    
+//    CAMediaTimingFunction *function = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    // [(0,0), c1, c2, (1,1)]为贝塞尔曲线控制点，第一、二参数表示(c1x, c1y)，第三、四参数表示(c2x, c2y)。
+//    CAMediaTimingFunction *function = [CAMediaTimingFunction functionWithControlPoints:0.8 :0.2 :1 :0.2]; // 前期很慢，后面很快
+    CAMediaTimingFunction *function = [CAMediaTimingFunction functionWithControlPoints:0.5 :-1 :1 :0]; // 前期增量为负值，从大约时间*0.4开始，开始增量为正，且增量越来越快。CAMediaTimingFunction的x轴表示时间，y轴表示fromValue到toValue的增量，y轴值为1表示到达toValue，为0.2表示到达fromValue+(toValue-fromValue)*0.2。
+    
+    
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position"];
+    animation.fromValue = [NSValue valueWithCGPoint:CGPointMake(85, 55)];
+    animation.toValue = [NSValue valueWithCGPoint:CGPointMake(UIScreen.mainScreen.bounds.size.width * 0.5, 55)];
+    animation.duration = 1;
+    animation.timingFunction = function;
+    animation.fillMode = kCAFillModeForwards;
+    animation.removedOnCompletion = NO;
+    [layer addAnimation:animation forKey:TempAnimationKey];
+}
+
+- (void)testCalculationMode {
+    CALayer *layer = [CALayer layer];
+    layer.name = TempLayerName;
+    layer.frame = CGRectMake(20, 50, 10, 10);
+    layer.backgroundColor = UIColor.redColor.CGColor;
+    [self.view.layer addSublayer:layer];
+    
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    animation.values = @[[NSValue valueWithCGPoint:CGPointMake(25, 55)],
+                         [NSValue valueWithCGPoint:CGPointMake(125, 55)],
+                         [NSValue valueWithCGPoint:CGPointMake(125, 155)]];
+//    animation.path = CGPathCreateWithRoundedRect(CGRectMake(20, 50, 200, 200), 100, 100, nil); // 设置calculationMode为paced，就不会在四个关键点有停顿，默认的linear会
+    animation.duration = 2;
+    animation.keyTimes = @[@0, @0.2, @1];
+//    animation.calculationMode = kCAAnimationDiscrete; // 位置跳变，且无最后一个点位置
+    animation.calculationMode = kCAAnimationPaced; // keyTimes设置被忽略
+//    animation.calculationMode = kCAAnimationCubic; // 路径像一个曲线
+    [layer addAnimation:animation forKey:TempAnimationKey];
 }
 
 @end
